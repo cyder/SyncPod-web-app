f00fcb81045c28508b99fbf35e316592bfed4762
Mori Atsushi <mori@cyder.tech>
2019-01-30 09:03:41 +0900

    Add url-loader and file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,28 @@ module.exports = {
         loader: 'source-map-loader'
       },
       {
+        test: /\.(png|jpg|gif)$/i,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 20000,
+            name: '[path][name].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.(eot|otf|ttf|woff2?|svg)(\?.+)?$/,
+        include: [
+            path.resolve(__dirname, 'node_modules')
+        ],
+        use: {
+            loader: 'file-loader',
+            options: {
+                name: '[path][name].[ext]'
+            }
+        }
+      },
+      {
         test: /\.mjs$/,
         include: /node_modules/,
         type: "javascript/esm",
