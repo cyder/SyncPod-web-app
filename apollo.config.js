3ef91d9f349cc924e462af300fc35ed2712d8857
Mori Atsushi <at.sushi.tb@gmail.com>
2019-03-23 14:36:43 +0900

    Change endpoint of graphql schema

--- apollo.config.js
+++ apollo.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   client: {
     service: {
-      name: 'syncpod',
-      url: 'http://localhost:4000/graphql',
+      name: 'syncpod-BFF',
+      url: 'https://engine.apollographql.com/service/syncpod-BFF/explorer',
     },
     includes: ['src/queries/**/*.{ts,tsx,graphql}'],
     addTypename: true,
