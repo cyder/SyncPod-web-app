12abfda0a3d7986e8628d73169501f8b7bdd6d7b
Mori Atsushi <at.sushi.tb@gmail.com>
2019-03-29 17:14:52 +0900

    Add ignore pattern of internal link

--- src/components/atoms/Links/InternalLink.tsx
+++ src/components/atoms/Links/InternalLink.tsx
@@ -9,10 +9,23 @@ interface Props {
 
 export default ({ to, children }: Props) => {
   const history = React.useContext(historyContext);
+  const isModifiedEvent = React.useCallback(
+    (e: React.MouseEvent<HTMLAnchorElement>): boolean =>
+      !!(e.metaKey || e.altKey || e.ctrlKey || e.shiftKey),
+    [],
+  );
   const onClick = React.useCallback(
     (e: React.MouseEvent<HTMLAnchorElement>) => {
-      history.push(to);
-      e.preventDefault();
+      if (
+        !e.defaultPrevented && // onClick prevented default
+        e.button === 0 && // ignore everything but left clicks
+        !isModifiedEvent(e) // ignore clicks with modifier keys
+      ) {
+        if (history.location.pathname !== to) {
+          history.push(to);
+        }
+        e.preventDefault();
+      }
     },
     [to],
   );
