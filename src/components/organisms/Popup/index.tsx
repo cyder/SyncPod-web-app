1609b062e3ae5ab2c39a9a95ece5fdea97079722
Mori Atsushi <at.sushi.tb@gmail.com>
2019-04-05 00:05:16 +0900

    Add simple styles of popup

--- src/components/organisms/Popup/index.tsx
+++ src/components/organisms/Popup/index.tsx
@@ -1,4 +1,6 @@
+/** @jsx jsx */
 import * as React from 'react';
+import { jsx, css } from '@emotion/core';
 
 import { PopupContext, PopupType } from 'components/providers/Context/popup';
 
@@ -24,5 +26,39 @@ export default () => {
   if (!component) {
     return null;
   }
-  return <div>{component}</div>;
+
+  return (
+    <div
+      css={css`
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.5);
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 1000;
+      `}
+      onClick={popupContext.close}
+      onKeyDown={() => {}}
+      role="presentation"
+    >
+      <div
+        onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}
+        onKeyDown={() => {}}
+        role="presentation"
+        css={css`
+          background-color: #ffffff;
+          cursor: auto;
+        `}
+      >
+        {component}
+      </div>
+    </div>
+  );
 };
