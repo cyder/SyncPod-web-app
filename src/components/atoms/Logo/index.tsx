e243a98ac8ec215c8aca0f012bb8e3ca69e983aa
tanishi <tanishi503@gmail.com>
2019-04-15 15:41:41 +0900

    Update height parametize

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -1,5 +1,22 @@
-import * as React from 'react';
+/** @jsx jsx */
 
+import { jsx, css } from '@emotion/core';
+
+// @ts-ignore
 import * as logo from 'components/atoms/Logo/logo.svg';
 
-export default () => ( <img src={logo} /> );
+interface Props {
+  readonly height?: string;
+}
+
+export default ({height = "50px"} : Props) => (
+  <img
+    css={css`
+      height: ${height};
+      object-fit: contain;
+      margin-left: -16px;
+      margin-right: auto;
+    `}
+    src={logo}
+  />
+);
