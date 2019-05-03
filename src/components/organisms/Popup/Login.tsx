1ca9bae27e5328343310a6310dd2695f4b3a4c37
Mori Atsushi <at.sushi.tb@gmail.com>
2019-05-04 03:36:14 +0900

    Add login mutation

--- src/components/organisms/Popup/Login.tsx
+++ src/components/organisms/Popup/Login.tsx
@@ -1,26 +1,70 @@
 /** @jsx jsx */
 
+import { useCallback, useState } from 'react';
 import { jsx } from '@emotion/core';
+import { useMutation } from 'react-apollo-hooks';
 
 import TextInput from 'components/atoms/Forms/TextInputWithLabel';
 import Form, { FormElement } from 'components/molecules/Forms/Form';
 
+import LoginMutation from 'queries/login';
+import { Login, LoginVariables } from 'queries/__generated__/Login';
+
 export default () => {
+  const login = useMutation<Login, LoginVariables>(LoginMutation);
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = useCallback(() => {
+    setLoading(true);
+    setEmailError(false);
+    setPasswordError(false);
+    setErrorMessage(undefined);
+    login({
+      variables: { email, password },
+    }).then(
+      result => {
+        setLoading(false);
+        console.log(result.data && result.data.login);
+      },
+      () => {
+        setLoading(false);
+        setEmailError(true);
+        setPasswordError(true);
+        setErrorMessage(
+          'メールアドレス、もしくはパスワードが間違っている可能性があります。',
+        );
+      },
+    );
+  }, [email, password]);
+
   const forms: FormElement[] = [
     {
       key: 'account_name',
       component: (
-        <TextInput label="アカウント名" placeholder="入力してください。" />
+        <TextInput
+          value={email}
+          onChange={setEmail}
+          label="メールアドレス"
+          type="email"
+          placeholder="入力してください。"
+          warning={emailError}
+        />
       ),
     },
     {
       key: 'password',
       component: (
         <TextInput
+          value={password}
+          onChange={setPassword}
           label="パスワード"
           placeholder="入力してください。"
           type="password"
-          warning
+          warning={passwordError}
         />
       ),
     },
@@ -29,8 +73,10 @@ export default () => {
   return (
     <Form
       forms={forms}
-      errorMessage="エラーメッセージが入ります。"
+      errorMessage={errorMessage}
       submitLabel="ログイン"
+      onSubmit={handleSubmit}
+      loading={loading}
     />
   );
 };
