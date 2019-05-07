a421330b782fd74c6302da41422328c58f9adc92
Mori Atsushi <at.sushi.tb@gmail.com>
2019-05-08 01:13:12 +0900

    Add create room popup

--- src/components/organisms/Popup/CreateRoom.tsx
+++ src/components/organisms/Popup/CreateRoom.tsx
@@ -2,6 +2,52 @@
 
 import { jsx } from '@emotion/core';
 
+import TextInput from 'components/atoms/Forms/TextInputWithLabel';
+import Form, { FormElement } from 'components/molecules/Forms/Form';
+import CheckBoxes from 'components/atoms/Forms/CheckBoxesWithLabel';
+
 export default () => {
-  return <div>Create Room</div>;
+  const forms: FormElement[] = [
+    {
+      key: 'name',
+      component: (
+        <TextInput label="ルーム名" placeholder="入力してください。" warning />
+      ),
+    },
+    {
+      key: 'description',
+      component: (
+        <TextInput
+          label="ルーム名"
+          placeholder="入力してください。"
+          warning
+          multiLine
+        />
+      ),
+    },
+    {
+      key: 'is-public',
+      component: (
+        <CheckBoxes
+          label="公開設定"
+          hint={
+            '一般公開にすると全てのユーザが、\n限定公開にするとルームURLもしくはルームKeyを\n知っているユーザのみが入室できます。'
+          }
+          items={[
+            { id: 'public', label: '一般公開' },
+            { id: 'private', label: '限定公開' },
+          ]}
+          name="is-public"
+        />
+      ),
+    },
+  ];
+
+  return (
+    <Form
+      forms={forms}
+      errorMessage="エラーメッセージが入ります。"
+      submitLabel="参加"
+    />
+  );
 };
