cbae0aca2777eaedd528df2070fc844b3fbc6ffa
Mori Atsushi <at.sushi.tb@gmail.com>
2019-05-12 21:42:04 +0900

    Change meta tag by page

--- src/components/pages/Room.tsx
+++ src/components/pages/Room.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 
+import Head from '@/components/atoms/Heads/Head';
 import Room from '@/components/organisms/Room';
 
 interface Props {
   roomKey: string;
 }
 
-export default ({ roomKey }: Props) => <Room roomKey={roomKey} />;
+export default ({ roomKey }: Props) => (
+  <div>
+    <Head pageTitle="ルーム名" pageDescription="【ルーム名】ルーム説明" />
+    <Room roomKey={roomKey} />
+  </div>
+);
---
+++ src/constants/strings.ts
@@ -0,0 +1,7 @@
+export default {
+  APP_TITLE: 'SyncPod',
+  APP_SUB_TITLE: '新しい動画視聴のカタチ。離れた人と一緒に動画を楽しもう。',
+  APP_DESCRIPTION:
+    '離れた人と一緒に動画を楽しむことができます。友人や恋人と同じ時間を共有したり、アーティストのファン同士で盛り上がったり、楽しみ方は自由自在。',
+  DOMAIN: 'https://app.sync-pod.com',
+};
