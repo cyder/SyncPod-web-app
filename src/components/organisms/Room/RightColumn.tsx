7899bd394edf62c283fea153318f3c7e4752a312
Mori Atsushi <at.sushi.tb@gmail.com>
2019-05-13 21:55:10 +0900

    Switch normal player when click chat tab

--- src/components/organisms/Room/RightColumn.tsx
+++ src/components/organisms/Room/RightColumn.tsx
@@ -1,32 +1,51 @@
 /** @jsx jsx */
 
 import { css, jsx } from '@emotion/core';
+import { useCallback, useContext, useMemo } from 'react';
 
 import Chat from '@/components/organisms/Chat';
 import PlayList from '@/components/organisms/PlayList';
-import Tab from '@/components/organisms/Tab';
+import { editingContet } from '@/components/organisms/Room/Provider';
+import Tab, { TabItem } from '@/components/organisms/Tab';
 
 interface Props {
   className?: string;
 }
 
-export default ({ className }: Props) => (
-  <div
-    className={className}
-    css={css`
-      padding: 2rem 0;
-      display: flex;
-      flex-direction: column;
-    `}
-  >
-    <Tab
-      tabs={[
-        { title: 'チャット', component: <Chat /> },
-        {
-          title: 'プレイリスト',
-          component: <PlayList />,
-        },
-      ]}
-    />
-  </div>
-);
+export default ({ className }: Props) => {
+  const { setIsEditing } = useContext(editingContet);
+  const chatTab = useMemo<TabItem>(
+    () => ({
+      title: 'チャット',
+      component: <Chat />,
+    }),
+    [],
+  );
+  const playlistTab = useMemo<TabItem>(
+    () => ({
+      title: 'プレイリスト',
+      component: <PlayList />,
+    }),
+    [],
+  );
+  const tabs = useMemo<TabItem[]>(() => [chatTab, playlistTab], []);
+
+  const handleTabChange = useCallback((index: number) => {
+    if (tabs[index] === chatTab) {
+      setIsEditing(false);
+    }
+  }, []);
+
+  return (
+    <div
+      className={className}
+      css={css`
+        padding: 2rem 0;
+        display: flex;
+        flex-direction: column;
+      `}
+    >
+      <Tab tabs={tabs} onChange={handleTabChange} />
+    </div>
+  );
+};
